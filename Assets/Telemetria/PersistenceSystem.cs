ab26e9fb1cfed6db4d79e64e154778d918dc5869
Jesús Ramos Rodríguez <jeramauni@gmail.com>
2021-05-09 17:32:31 +0200

    Persistencia 1.0

--- Assets/Telemetria/PersistenceSystem.cs
+++ Assets/Telemetria/PersistenceSystem.cs
@@ -3,82 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-/*
-
- //Gestion de nivel :
-    InicioSesion,
-    InicioNivel,
-    FinSesion,
-    FinNivel,
-    Reinicio, array 
-    Pausa,    array 
-    //Recogida :
-    Coleccionable,
-    FotoRecogida,
-    FlashRecogida,
-    //Uso de :
-    BotonCamara,
-    FotoUso,
-    FlashUso,
-    FotoGuardia
-
-public struct TelemetryEvent
-{
-    public TipoEvento type;
-    public DateTime time;
-    public int nivel;
-    public void setNull() { time = -1.0f; }
-}
-*/
-
-/*
-Estructura:
-"Inicio timestamp": "19/06/2015 17:35:50",
-"Fin timestamp": 
-[
-    "19/06/2015 17:35:50",
-    "19/06/2015 17:35:50",
-    "19/06/2015 17:35:50" 
-],
-"Reinicio timestamp": 
-[
-    "19/06/2015 17:35:50",
-    "19/06/2015 17:35:50"
-],
-"Pausa timestamp": 
-[
-    "19/06/2015 17:35:50"
-],
-"Salida timestamp": "19/06/2015 17:35:50",
-"Coleccionables recogidos": 4,
-...
-
-NIVEL 1
-
-
-"Coleccionable":  " lvl 1, 19/06/2015 17:35:50",
-*/
-
 public class PersistenceSystem 
 {
     #region FileManager (Opening and closing I/O)
 
     static string FILEPATH = @".\Telemetria\session_id.txt";
 
-    StreamReader sr = null;
-    StreamWriter sw = null;
+    private StreamWriter sw = null;
 
-    public bool OpenReadFile(ref string file_path) //Opens output file (writting) if it isn't already openned
-    {
-        if (!File.Exists(file_path))
-        {
-            sr = File.OpenText(file_path);
-            return true;
-        }
-        return false;
-    }
-
-    public bool OpenWriteFile (ref string file_path) //Opens output file (writting) if it isn't already openned
+    private bool OpenWriteFile (string file_path) //Opens output file (writting) if it isn't already openned
     {
         if (!File.Exists(file_path))
         {
@@ -87,74 +20,82 @@ public class PersistenceSystem
         }
         return false;
     }
+
+    public bool Init ()
+    {
+        return OpenWriteFile(FILEPATH);
+    }
+
+    public bool ShutDown()
+    {
+        sw.Close();
+        return true;
+    }
     #endregion
 
-    #region Data (Last known register)
-    string sInicioSesion = "";
-    string sInicioNivel = "";
-    string sFinSesion = "";
-    string sFinNivel = "";
-    string sReinicio = "";
-    string sPausa = "";
-    string sColeccionable = "";
-    string sFotoRecogida = "";
-    string sFlashRecogida = "";
-    string sBotonCamara = "";
-    string sFotoUso = "";
-    string sFlashUso = "";
-    string sFotoGuardia = "";
-
-    //Aux
-    string arrayCloser = "\n],\n";
+    #region Printer
+    private bool PrintOnDefault (ref string data)
+    {
+        sw.Write(data);
+        return true;
+    }
     #endregion
 
     #region Encoder
     public bool toJson(TelemetryEvent e)
     {
-        string result = "";
+        string result = " ";
         switch (e.type)
         {
             case TipoEvento.InicioSesion:
-                result = "\"Inicio sesion timestamp\": " + '\"' + e.time.ToString() + "\",\n";
-                sInicioSesion = result;
+                result = "\"Inicio sesion\": " + '\"' + "lvl " + e.nivel.ToString() + ' ' + e.time.ToString() + "\",\n";
                 break;
             case TipoEvento.InicioNivel:
-                result = "\"Inicio nivel timestamp\": " + '\"' + e.time.ToString() + "\",\n";
-                sInicioNivel = result;
+                result = "\"Inicio nivel\": " + '\"' + "lvl " + e.nivel.ToString() + ' ' + e.time.ToString() + "\",\n";
                 break;
             case TipoEvento.FinSesion:
-                if (first)
-                    result = "\"Fin timestamp\": \n[";
+                result = "\"Fin sesion\": " + '\"' + "lvl " + e.nivel.ToString() + ' ' + e.time.ToString() + "\",\n";
                 break;
             case TipoEvento.FinNivel:
-                if (first)
-                    result = "\"Fin timestamp\": \n[";
+                result = "\"Fin nivel\": " + '\"' + "lvl " + e.nivel.ToString() + ' ' + e.time.ToString() + "\",\n";
                 break;
             case TipoEvento.Reinicio:
+                result = "\"Reinicio de nivel\": " + '\"' + "lvl " + e.nivel.ToString() + ' ' + e.time.ToString() + "\",\n";
                 break;
             case TipoEvento.Pausa:
+                result = "\"Pausa\": " + '\"' + "lvl " + e.nivel.ToString() + ' ' + e.time.ToString() + "\",\n";
                 break;
             case TipoEvento.Coleccionable:
+                result = "\"Coleccionable recogido\": " + '\"' + "lvl " + e.nivel.ToString() + ' ' + e.time.ToString() + "\",\n";
                 break;
             case TipoEvento.FotoRecogida:
+                result = "\"Fotografia recogida\": " + '\"' + "lvl " + e.nivel.ToString() + ' ' + e.time.ToString() + "\",\n";
                 break;
             case TipoEvento.FlashRecogida:
+                result = "\"Flash recogido\": " + '\"' + "lvl " + e.nivel.ToString() + ' ' + e.time.ToString() + "\",\n";
                 break;
             case TipoEvento.BotonCamara:
+                result = "\"Boton camara pulsado\": " + '\"' + "lvl " + e.nivel.ToString() + ' ' + e.time.ToString() + "\",\n";
                 break;
             case TipoEvento.FotoUso:
+                result = "\"Fotografia usada\": " + '\"' + "lvl " + e.nivel.ToString() + ' ' + e.time.ToString() + "\",\n";
                 break;
             case TipoEvento.FlashUso:
+                result = "\"Flash usado\": " + '\"' + "lvl " + e.nivel.ToString() + ' ' + e.time.ToString() + "\",\n";
                 break;
             case TipoEvento.FotoGuardia:
-                break;
-            case TipoEvento.Null:
+                result = "\"Guardia fotografiado\": " + '\"' + "lvl " + e.nivel.ToString() + ' ' + e.time.ToString() + "\",\n";
                 break;
             default:
                 break;
         }
 
-        return true;
+        if (result != " ")  //Print
+        {
+            return PrintOnDefault(ref result);
+        }
+
+        else return false;
     }
     #endregion
 }
